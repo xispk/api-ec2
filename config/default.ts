595182e4bc9a0b326facb1eb90290c92c68e5fc1
iuryBIOS <iury@bios.inf.br>
2022-03-06 20:55:57 -0300

    default config update

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 export default {
   port: 1337,
   host: 'http://localhost',
-  dbUrl: 'mongodb+srv://iury:1234@cluster0.uinup.mongodb.net/api-ec2',
+  dbUrl: '',
   senderMail: 'test@example.com',
   access_token_sign_options: { expiresIn: '15m', algorithm: 'RS256' },
   refresh_token_sign_options: { expiresIn: '1y', algorithm: 'RS256' },
