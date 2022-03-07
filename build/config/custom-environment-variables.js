fc649224a7fac12a5b112587694b1d340894d787
iuryBIOS <iury@bios.inf.br>
2022-03-06 21:19:33 -0300

    build without envs

--- build/config/custom-environment-variables.js
+++ build/config/custom-environment-variables.js
@@ -5,4 +5,6 @@ exports.default = {
     access_token_public_key: 'ACCESS_TOKEN_PUBLIC_KEY',
     refresh_token_private_key: 'REFRESH_TOKEN_PRIVATE_KEY',
     refresh_token_public_key: 'REFRESH_TOKEN_PUBLIC_KEY',
+    dbUrl: 'MONGO_URL',
+    host: 'HOST',
 };
--- build/config/default.js
+++ build/config/default.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     port: 1337,
     host: 'http://localhost',
-    dbUrl: 'mongodb+srv://iury:1234@cluster0.uinup.mongodb.net/api-ec2',
+    dbUrl: '',
     senderMail: 'test@example.com',
     access_token_sign_options: { expiresIn: '15m', algorithm: 'RS256' },
     refresh_token_sign_options: { expiresIn: '1y', algorithm: 'RS256' },
